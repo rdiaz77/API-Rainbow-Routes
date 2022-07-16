aba8a9c310c6f383081bf7a6706d8a5e846805bc
RDM_ <rafadiazm77@gmail.com>
2022-07-16 12:53:37 -0500

    activity

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 // Require needed node modules
-const express = require('express')
+import express from 'express'
 
 // Initialize your application by calling the function returned by the express module
 const app = express()
@@ -10,7 +10,7 @@ const app = express()
 // Declare routes that people can visit on the application
 
 // White Page
-app.get('/', function (req, res) {
+app.get('/', function (req: express.Request, res: express.Response): void {
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: white;">
@@ -24,7 +24,8 @@ app.get('/', function (req, res) {
 
 // Color Page
 app.get('/:color', function (req, res) {
-    let myColor = req.params.color
+    let myColor: unknown = req.params.color
+    if( typeof(myColor) === "string")
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
